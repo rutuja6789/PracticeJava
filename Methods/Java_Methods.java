ecb20cd02ce1808b32a89ee9df9ba5f34e7c4521
rutuja6789 <ashtulerutuja99@gmail.com>
2024-10-25 21:34:56 +0100

    Update methods

--- Methods/Java_Methods.java
+++ Methods/Java_Methods.java
@@ -2,6 +2,12 @@ package Methods;
 
 public class Java_Methods {
     public static void main(String[] args) {
-
+        int AvgOfNums = AvgNum(2,6);
+        System.out.println(AvgOfNums+10);
+    }
+    public static int AvgNum(int a, int b){
+        int avg = (a+b)/2;
+        System.out.println("Average of 2 numbers is = "+ avg);
+        return avg;
     }
 }
