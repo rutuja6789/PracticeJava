833c5209d62c54583a05b5a0ae1dc9043d9a6461
rutuja6789 <ashtulerutuja99@gmail.com>
2024-10-25 21:45:04 +0100

    Learn math methods

---
+++ Methods/MathFunc.java
@@ -0,0 +1,13 @@
+package Methods;
+
+public class MathFunc {
+    public static void main(String[] args) {
+
+        int a = -592;
+        int b = 12;
+
+        System.out.println("min number is = " + Math.min(a,b));
+        System.out.println("max number is = " + Math.max(a,b));
+        System.out.println("abs number is = " + Math.abs(a));
+    }
+}
