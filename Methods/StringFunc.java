a1b69d4304692f5e1df96aaa6e396a941ba64aa3
rutuja6789 <ashtulerutuja99@gmail.com>
2024-10-26 18:54:38 +0100

    Update

---
+++ Methods/StringFunc.java
@@ -0,0 +1,11 @@
+package Methods;
+
+public class StringFunc {
+    public static void main(String[] args) {
+        String name = "  Rutuja   ";
+
+        System.out.println(name.toUpperCase());
+        System.out.println(name.toLowerCase());
+        System.out.println(name.trim());
+    }
+}
